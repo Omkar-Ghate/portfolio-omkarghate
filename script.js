35b94cd136e315827a9f026685aad7e7c25c1edf
Omkar Ghate <otg0231@gmail.com>
2021-06-25 22:41:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -150,8 +150,8 @@ const projects = [
     projectDescription:
       "Designed & Built the Portfolio using HTML CSS and Vanilla JavaScript to show case the Professional Journey.",
     projectImage: "images/portfolioWebsite.jpg",
-    codeLink: "https://github.com/smilegupta/portfolio-smilegupta",
-    demoLink: "https://smilegupta.github.io/portfolio-smilegupta/"
+    codeLink: "https://portfolio-omkarghate.vercel.app/",
+    demoLink: "https://portfolio-omkarghate.vercel.app/"
   },
 
   {
